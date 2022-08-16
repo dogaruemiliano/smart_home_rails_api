8f52c495a7fd1321dd1d40aad52f82ec61e955b0
dogaruemiliano <dogaru.emiliano@gmail.com>
2022-08-16 18:09:37 +0200

    Added policy for AirConditionersController

---
+++ app/policies/air_conditioner_policy.rb
@@ -0,0 +1,20 @@
+class AirConditionerPolicy < ApplicationPolicy
+  # class Scope < Scope
+  #   # NOTE: Be explicit about which records you allow access to!
+  #   # def resolve
+  #   #   scope.all
+  #   # end
+  # end
+
+  def get_state?
+    true
+  end
+
+  def controll?
+    if record.owner_only
+      user == record.user
+    else
+      user
+    end
+  end
+end
